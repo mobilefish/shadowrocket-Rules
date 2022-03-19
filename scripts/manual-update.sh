143c32bf606ae39e22fd2d19830629d4bcd10926
Chien <xiangjianjian@hotmail.com>
2022-03-19 10:52:30 +0800

    manual update

--- scripts/manual-update.sh
+++ scripts/manual-update.sh
@@ -8,7 +8,7 @@ python3 ./scripts/build-direct.py
 python3 ./scripts/build-proxy.py
 python3 ./scripts/build-netease.py
 python3 ./scripts/build-node.py
-python3 ./scripts/merge-lhie1.py
+# python3 ./scripts/merge-lhie1.py
 python3 ./scripts/gen-final.py
 
 git add .
