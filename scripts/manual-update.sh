0a65bc6dd2c31b82814f571b96c29d01b07e178c
Chien <xiangjianjian@hotmail.com>
2022-05-02 15:40:27 +0800

    manual update

--- scripts/manual-update.sh
+++ scripts/manual-update.sh
@@ -4,9 +4,13 @@ git pull
 
 
 python ./scripts/build-ad.py
+python ./scripts/build-telegram.py
+python ./scripts/build-direct.py
+python ./scripts/build-proxy.py
 python ./scripts/build-netease.py
 python ./scripts/build-node.py
 python ./scripts/build-sgmodule.py
+python ./scripts/gen-final.py
 
 git add .
 git commit -m "manual update" -a
