d4bdafdae5657eb573266cf1c52e1c199162b8dc
Chien <xiangjianjian@hotmail.com>
2022-08-31 19:43:46 +0800

    ximalaya: rewrite only

--- scripts/build-ximalaya.py
+++ scripts/build-ximalaya.py
@@ -51,21 +51,21 @@ if __name__ == '__main__':
     ximalaya.write("  rewrite:\n")
     for rewrite in rewrites:
         ximalaya.write("    - {} - reject\n".format(rewrite))
-    ximalaya.write("  script:\n")
-    for script in scripts:
-        script_url = script["script_url"]
-        script_url_set.add(script_url)
-        script_name = get_script_name(script_url)
-        ximalaya.write("    - match: {}\n".format(script["url"]))
-        ximalaya.write("      name: {}\n".format(script_name))
-        ximalaya.write("      type: response\n")
-        ximalaya.write("      require-body: true\n")
-        ximalaya.write("      timeout: 30\n")
-    for script_url in script_url_set:
-        script_name = get_script_name(script_url)
-        ximalaya.write("script-providers:\n")
-        ximalaya.write("  {}:\n".format(script_name))
-        ximalaya.write("    url: {}\n".format(script_url))
-        ximalaya.write("    interval: 86400\n")
+    # ximalaya.write("  script:\n")
+    # for script in scripts:
+    #     script_url = script["script_url"]
+    #     script_url_set.add(script_url)
+    #     script_name = get_script_name(script_url)
+    #     ximalaya.write("    - match: {}\n".format(script["url"]))
+    #     ximalaya.write("      name: {}\n".format(script_name))
+    #     ximalaya.write("      type: response\n")
+    #     ximalaya.write("      require-body: true\n")
+    #     ximalaya.write("      timeout: 30\n")
+    # for script_url in script_url_set:
+    #     script_name = get_script_name(script_url)
+    #     ximalaya.write("script-providers:\n")
+    #     ximalaya.write("  {}:\n".format(script_name))
+    #     ximalaya.write("    url: {}\n".format(script_url))
+    #     ximalaya.write("    interval: 86400\n")
     ximalaya.close()
     print('The stash override has been convert successfully!')
