90d271746b2fffb6ba38b21e61e2a3febace9c54
Chien <xiangjianjian@hotmail.com>
2022-09-09 11:03:49 +0800

    update cug.js

--- scripts/cug.js
+++ scripts/cug.js
@@ -16,9 +16,12 @@ if (url.endsWith('%E5%AE%A1%E6%A0%B8%E9%80%9A%E8%BF%87') || url.endsWith('all'))
         date = "0" + date;
     }
     var dateStr = year + "-" + month + "-" + date;
+    var dateSugStr = year + " " + month + " " + (date - 1);
 
-    json.rows[0].outtimetext = dateStr + ' 08:35:59'
+    json.rows[0].outtimetext = dateStr + ' 08:35:59';
     json.rows[0].backtimetext = dateStr + ' 23:35:59';
+    json.rows[0].verifier1text = '审批通过';
+    json.rows[0].verifier2suggestiontext = ' 审批人：李欢欢，审批时间：' + dateSugStr + ' 16:04:18';
 }
 
 $done({ body: JSON.stringify(json) });
